f4319d763b4dcf073f6dabf6395cd39871805ec6
FirlanGrasp <chenshan_email.foxmail.com>
2026-01-31 13:12:13 +0800

    如果某题目只关联了已经被删除的测验，那么依旧返回false；只有关联了未被删除的测验的题目才返回true

--- src/main/java/com/yupi/yuoj/mapper/TestQuestionMapper.java
+++ src/main/java/com/yupi/yuoj/mapper/TestQuestionMapper.java
@@ -5,16 +5,12 @@ import com.yupi.yuoj.model.entity.TestQuestions;
 import org.apache.ibatis.annotations.Param;
 
 /**
-* 针对表【test_question(测验表)】的数据库操作Mapper
-*/
+ * 针对表【test_question(测验表)】的数据库操作Mapper
+ */
 public interface TestQuestionMapper extends BaseMapper<TestQuestions> {
 
     /**
-     * 统计某个题目被多少个测验关联（仅统计未删除记录）
+     * 统计某个题目被多少个未删除的测验关联（仅统计 test_question 未删除且测验 test 未删除）
      */
     int countByQuestionId(@Param("questionId") Long questionId);
 }
-
-
-
-
